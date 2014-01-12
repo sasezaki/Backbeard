eb05c33ace6d2bf79e03b569a2caa61f465c2753
sasezaki <sasezaki@gmail.com>
2014-01-12 20:27:54 +0900

    add test more

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -22,5 +22,20 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
         $request->setUri('/');
         $response = (new Dispatcher(['/' => function(){return 'hello';}]))->dispatch($request);
         $this->assertSame('hello', $response->getContent());
+
+        $request = new \Zend\Http\PhpEnvironment\Request;
+        $request->setUri('/foo/bar');
+        $response = (new Dispatcher(['/foo/:bar' => function($bar){return $bar;}]))->dispatch($request);
+        $this->assertSame('bar', $response->getContent());
+    }
+
+    public function testRoutingKeyHandlingArray()
+    {
+        $request = new \Zend\Http\PhpEnvironment\Request;
+        $request->setUri('/foo/bar');
+        $response = (new Dispatcher(call_user_func(function() {
+            yield ['route' => '/foo/:bar'] => function($bar){return $bar;};
+        })))->dispatch($request);
+        $this->assertSame('bar', $response->getContent());
     }
 }
